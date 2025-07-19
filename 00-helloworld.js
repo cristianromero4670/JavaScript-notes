1a6c6d45e0a4526f19fc00576b79932b719b41cc
Eddy <cristian.romero4670@alumnos.udg.mx>
2025-07-18 18:44:36 -0600

    inicio del proyecto

---
+++ 00-helloworld.js
@@ -0,0 +1,23 @@
+//comentario simple
+
+/* comentario
+en varias 
+linesas shift+alt+a*/
+
+console.log("Hello world!")
+console.log(`Hello world!`)
+console.log('Hello world!')
+
+/* console.log("Hello world!") */
+
+/* console.log("Hello world!")
+console.log(`Hello world!`)
+console.log('Hello world!') */
+
+console.log("5")
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
---
+++ 01-variables.js
@@ -0,0 +1,12 @@
+// var 
+
+var helloWorld = "Hello javascript"
+console.log(helloWorld)
+
+helloWorld = "Hello de nuevo, javascript"
+console.log(helloWorld)
+
+
+//let 
+
+//const 

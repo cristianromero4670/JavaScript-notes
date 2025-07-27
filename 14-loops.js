c7d908daf43466cca37cd7010caecd22ad5dc8e2
Eddy <cristian.romero4670@alumnos.udg.mx>
2025-07-27 10:57:03 -0600

    loops

---
+++ 14-loops.js
@@ -0,0 +1,77 @@
+// Loops o bucles 
+
+// for
+
+for (let i = 0; i < 5; i++) {
+    console.log(`Hola ${i}`)
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(`Elemento ${numbers[i]}`)
+}
+
+// while 
+
+let i = 6
+
+while(i < 5) {
+    console.log(`Hola ${i}`)
+    i++
+}
+//bucle infinito
+/* while(true) {
+} */
+
+//do while
+
+i = 6
+
+do{
+    console.log(`Hola ${i}`)
+    i++
+}while(i < 5)
+
+// for of
+
+//sirve para recorrer cada valor de las estructuras de datos 
+
+myArray = [1, 2, 3, 4]
+mySet = new Set(["Eduardo", "Romero", "Eddy", 22, true, "chr4259@gmail.com"])
+myMap = new Map([
+    ["name","Eddy"],
+    ["email","chr4259@gmail.com"],
+    ["age", 22]
+])
+
+let myString = "Hola javaScript"
+
+for (let valor of myArray) { // valor (declarar el valor que en el que va el recorrido) y of myArray (la estructura de datos que se esta recorriendo)
+    console.log(valor)
+}
+
+for (let valor of mySet) {
+    console.log(valor)
+}
+
+for (let valor of myMap) {
+    console.log(valor)
+}
+
+for (let valor of myString) {
+    console.log(valor)
+}
+
+// Buenas practicas 
+
+//revisar que no se crea un bucle infinito
+// break y continue 
+
+for(let i = 5; i < 10; i++){
+    if (i == 5) {
+        continue //se le indica que continue en la siguiente ejecucion y se salta esta vuelta
+    } else if(i == 7){
+        break //finaliza el recorrido del bucle. Se interrumpe en ese elemento
+    }
+}
---
+++ 15-loops-exercises.js
@@ -0,0 +1,19 @@
+// 1. Crea un bucle que imprima los numeros del 1 al 20
+
+// 2. crea un bucle que sume todos los numeros del 1 al 100 y muestre el resultado
+
+// 3. Crea un bucle  que imprima todos los numeros pares entre 1 y 50
+
+// 4. dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
+
+// 5. Escribe un bucle que cuente el numero de vocales en una cadena de texto
+
+// 6. Dado un array de numeros, usa un bucle para multiplicar todos los numeros y mostrar el producto 
+
+// 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+
+// 8. Usa un bucle para invertir una cadena de texto 
+
+// 9. Usa un bucle para generar los primeros 10 numeros de la secuencia Fibonacci
+
+// 10. Dado un array de numeros, usa un bucle para crear un nuevo array que contenga solo los numeros mayores a 10

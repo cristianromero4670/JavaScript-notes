3ee42c54a6ffbf554d6629d042481d54b2cf4a6d
Eddy <cristian.romero4670@alumnos.udg.mx>
2025-08-04 18:19:06 -0600

    ejercicios de funciones

--- 17-functions-exercises.js
+++ 17-functions-exercises.js
@@ -4,8 +4,7 @@ function suma(c, d){
     return c + d
 }
 
-result = suma(5, 10)
-console.log(result)
+console.log(suma(5, 10))
 
 // 2. Crea una funcion que reciba un array de numeros y devuelva el mayor de ellos
 
@@ -16,7 +15,8 @@ let cont = 0
 
 myArray.forEach(function (value) {
 
-    cont++
+cont++
+
 if(value >= mayor) {
     mayor = value
 }
@@ -70,19 +70,81 @@ console.log(esPrimo(num))
 
 // 6. Crea una funcion que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
 
+myArray = ["Eduardo", "Romero", "Eddy", 22, true]
+myArray2 = ["Eduardo", "Romero", 22, true]
 
+i = 0
 
 function arrayComun(array1, array2) {
-    let elemento = 0 
-    while (elemento ) {
+    let nuevaArray = []
 
-    }
+    array1.forEach(function (value) {
+        if (array2.includes(value) && !nuevaArray.includes(value)) {
+            nuevaArray.push(value)
+        }
+    })
+    return nuevaArray
 }
+
+
+console.log(arrayComun(myArray, myArray2))
+
+
 
 // 7. Crea una funcion que reciba una array de dos numeros y devuelva la suma de todos los numeros pares 
 
+let numeros = [34, 55]
+total = 0
+
+function sumaPares(array) {
+
+    for (i = array[0]; i != array[1]; i++) {
+        if (i % 2 == 0) {
+            total += i
+        }
+    }
+    return total
+}
+
+console.log(sumaPares(numeros))
+
 // 8. Crea una funcion que reciba un array de numeros y devuelva un nuevo array con cada numero elevado al cuadrado
+
+myArray = [45, 22, 5, 45, 2, 1, 30]
+nuevaArray = []
+
+myArray.forEach(function (value) {
+    nuevaArray.push(value**2)
+})
+
+console.log(nuevaArray)
 
 // 9. Crea una funcion que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
 
+cad = "cadena de texto que debe tener orden inverso"
+
+function cadenaInvertida(string) {
+    let separados = string.split(" ")
+    let invertido = separados.reverse()
+    string = invertido.join(" ")
+
+    return string
+}
+
+console.log(cadenaInvertida(cad))
+
 // 10. Crea una funcion que calcule el factorial de un numero dado
+
+let number = 5
+total = 1
+
+function factorial(numero) {
+
+    while(numero > 1){
+        total *= numero
+        numero--
+    }
+    return total
+}
+
+console.log(factorial(number))

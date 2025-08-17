01ae3e53dd3bd37e54074759b892cf2923229d5f
Eddy <cristian.romero4670@alumnos.udg.mx>
2025-08-16 18:53:32 -0600

    ejercicios de objetos

--- 21-destructuring-spreading-exercises.js
+++ 21-destructuring-spreading-exercises.js
@@ -9,20 +9,60 @@ console.log(secondElement); // 20
 
 // 2. Usa desestructuracion en una array y asigna un valor predeterminado a una variable
 
-
+let [element, , , , ] = otroArray
+console.log(element) // 10
 
 // 3. Usa desestructuracion para extraer dos propiedades de un objeto
 
+let school = {
+    name: 'CUCEI',
+    carrer: 'computer ingeneering',
+    year: 2022,
+    location: 'guadalajara',
+    student: {
+        numerStudent: 2184,
+        age: 22,
+        name: 'Eduardo'
+    }
+}
+
+let {carrer, location: ubicacion} = school
+console.log(carrer, ubicacion); // computer ingeneering guadalajara
+
 // 4. Usa desestructuracion para extraer dos propiedades de un objeto y asignalas a nuevas variables con nombres diferentes
+
+let {name: schoolName, year: yearEntry} = school
+console.log(schoolName, yearEntry)
 
 // 5. Usa desestructuracion para extraer dos propiedades de un objeto anidado
 
+let {student: {numerStudent: code}, student: {name: nameStudent}} = school
+console.log(code, nameStudent)
+
 // 6. Usa propagacion para combinar dos arrays en uno nuevo
+
+let nuevoArray = [...otroArray, ...myArray]
+console.log(nuevoArray)
 
 // 7. Usa propagacion para crear una copia de una array
 
+let finalArray = [...otroArray]
+console.log(finalArray)
+
 // 8. Usa propagacion para combinar dos objetos en uno nuevo
+
+let newSchool = {...person, ...school}
+console.log(newSchool)
 
 // 9. Usa propagacion para crear una copia de un objeto
 
+let finalSchool = {...newSchool}
+console.log(finalSchool)
+
 // 10. Combina desestructuracion y propagacion 
+let ultimoArray = []
+let [combination, text, ofArray] = myArray
+
+ultimoArray.push(combination, text, ofArray)
+
+console.log(ultimoArray)
